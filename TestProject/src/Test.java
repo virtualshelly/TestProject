aa8c2adbb26f09623ac997d51d977e2444187a62
Sleeping Beauty <Sleeping Beauty@Blue>
2021-07-13 18:01:14 -0400

    Added Try Catch

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,7 +2,19 @@
 public class Test {
 
 	public static void main(String[] args) {
-
+		
+		// testing try catch
+		try {
+			System.out.println("Hello World!");
+			int i = 1/0;
+			System.out.println("Completed");
+		}
+		catch(Exception exp) {
+			System.out.println("I am inside the catch block");
+			System.out.println("Message is: "+exp.getMessage());
+			System.out.println("Cause is " +exp.getCause());
+			exp.printStackTrace();
+		}
 
 	}
 
